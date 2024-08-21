53b4e3442d8580a2bef8e5e6d07649769fe740da
Aditya Dave <adityardave300@gmail.com>
2024-08-21 23:13:08 +0530

    My first app

--- NextJs/my-first-next-app/app/page.js
+++ NextJs/my-first-next-app/app/page.js
@@ -1,31 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <h1>
+          <Link href="/custom_function">
+            {" "}
+            New page in nextjs without loading
+          </Link>
+        </h1>
       </div>
 
       <div className={styles.center}>
