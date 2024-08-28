7060a55d039fc437f6420787d3d28858fd38e185
Aditya Dave <adityardave300@gmail.com>
2024-08-28 23:35:01 +0530

    Saving meal to the db

--- NextJs/codeforfood/lib/actions.js
+++ NextJs/codeforfood/lib/actions.js
@@ -1,5 +1,7 @@
 "use server";
 
+import { saveMeal } from "./meals";
+
 export async function shareMeal(formdata) {
   const meal = {
     creator: formdata.get("name"),
@@ -8,7 +10,8 @@ export async function shareMeal(formdata) {
     summary: formdata.get("summary"),
     instructions: formdata.get("instructions"),
     image: formdata.get("image"),
+    slug: "",
   };
 
-  console.log(meal);
+  await saveMeal(meal);
 }
--- NextJs/codeforfood/lib/meals.js
+++ NextJs/codeforfood/lib/meals.js
@@ -1,4 +1,7 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
+const fs = require("fs");
 const db = sql("meals.db");
 
 export default async function getMeals() {
@@ -9,4 +12,42 @@ export default async function getMeals() {
 
 export async function getMeal(slug) {
   return db.prepare("Select * from meals where slug = ?").get(slug);
+}
+
+export async function saveMeal(meals) {
+  // forces the meal title to be lower cased and slugified by the module.
+  meals.slug = slugify(meals.title, { lower: true });
+  // Sanitize user input
+  meals.instructions = xss(meals.instructions);
+  // extension of the image file
+  const extension = meals.image.name.split(".").pop();
+  // filename of the image to be stored.
+  const filename = `${meals.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${filename}`);
+  // create an image buffer to write to.
+  const bufferData = await meals.image.arrayBuffer();
+
+  // write image from the buffer data
+  stream.write(Buffer.from(bufferData), (err) => {
+    if (err) {
+      console.log("Saving image failed !!");
+    }
+  });
+  meals.image = `images/${filename}`;
+
+  console.log(meals);
+  db.prepare(
+    `
+      INSERT INTO meals VALUES (
+         null,
+         @slug,
+         @title,
+         @image,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email
+      )`
+  ).run(meals);
 }
