432dc8e16fdea765cfb4ec1c3a5f361e9cf197b5
Aditya Dave <adityardave300@gmail.com>
2025-04-05 20:33:11 +0530

    Changed params word to context

--- NextJs/nexttodolist/app/api/todos/[userid]/route.ts
+++ NextJs/nexttodolist/app/api/todos/[userid]/route.ts
@@ -2,10 +2,10 @@ import Todo from '@/models/todo';
 import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '@/lib/db';
 
-export async function GET(request: NextRequest, { params }:{ params: {userid : string}}) {
+export async function GET(request: NextRequest, context:{ params: {userid : string}}) {
   try {
     await dbConnect();
-    const { userid} = params;
+    const userid = context.params.userid;
 
     const userTodos = await Todo.find({ userName: userid });
 
@@ -22,10 +22,10 @@ export async function GET(request: NextRequest, { params }:{ params: {userid : s
   }
 }
 
-export async function POST(request: NextRequest, { params }:{ params: {userid : string}}) {
+export async function POST(request: NextRequest, context:{ params: {userid : string}}) {
   try {
     await dbConnect();
-    const { userid} = params;
+    const userid = context.params.userid;
     const { title, description } = await request.json();
 
     const newTodo = new Todo({
@@ -47,10 +47,10 @@ export async function POST(request: NextRequest, { params }:{ params: {userid : 
   }
 }
 
-export async function PATCH(request: NextRequest, { params }:{ params: {userid : string}}) {
+export async function PATCH(request: NextRequest, context:{ params: {userid : string}}) {
   try {
     await dbConnect();
-    const { userid} = params;
+    const userid = context.params.userid;
     const { id, title, description, completed } = await request.json();
 
     const updatedTodo = await Todo.findByIdAndUpdate(
@@ -72,10 +72,10 @@ export async function PATCH(request: NextRequest, { params }:{ params: {userid :
   }
 }
 
-export async function DELETE(request: NextRequest, { params }:{ params: {userid : string}}) {
+export async function DELETE(request: NextRequest, context:{ params: {userid : string}}) {
   try {
     await dbConnect();
-    const { userid} = params;
+    const userid = context.params.userid;
     const { id } = await request.json();
     const userTodos = await Todo.findByIdAndDelete({ _id: id });
 
