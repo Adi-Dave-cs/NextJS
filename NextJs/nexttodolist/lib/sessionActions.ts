9e24612c7cfb3cacda497784c46b7deffa5eb2d2
Aditya Dave <adityardave300@gmail.com>
2025-04-13 14:29:31 +0530

    Commit for vercel

--- NextJs/nexttodolist/lib/sessionActions.ts
+++ NextJs/nexttodolist/lib/sessionActions.ts
@@ -45,15 +45,14 @@ export function getUserFromSession(cookies: Pick<Cookies,'get'>)
 {
     const sessionid = cookies.get('session_identifier')?.value;
     if(!sessionid) return null;
-    console.log('Session ',sessionid);
     return getUserSessionByid(sessionid);
 }
 
 export async function getUserSessionByid(sessionid : string)
 {
     const rawUser = await redisClient.get(`session:${sessionid}`);
-    console.log("RawUser",rawUser);
     if(isValidSession(rawUser)) return rawUser;
+    console.log("RawUser",rawUser);
     return null;
 }
 
