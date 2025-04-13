609d13c24d719f92280605544627a064a3ea6099
Aditya Dave <adityardave300@gmail.com>
2025-04-14 01:24:13 +0530

    Added admin page as a client

--- NextJs/nexttodolist/app/admin/page.tsx
+++ NextJs/nexttodolist/app/admin/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import UserComponent from "@/components/UserComponent/UserComponent";
 
 
 export default async function Admin()
 {
-    const users = await fetch(`${process.env.HOST_URL}/api/admin/`,{method : 'GET'}).then(res => res.json());
+    const users = await fetch(`/api/admin/`,{method : 'GET'}).then(res => res.json());
     return <>
     <div className="w-full h-full">
     {
